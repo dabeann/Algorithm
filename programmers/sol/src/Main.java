142275f3931f462d84e152a3767b6b2d07f5167a
dabeann <dabin4078@daum.net>
2024-01-14 12:29:05 +0900

    feat: n의 배수 고르기

--- programmers/sol/src/Main.java
+++ programmers/sol/src/Main.java
@@ -2,6 +2,8 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 // 알고리즘 문제 형식
+
+/*
 class Solution {
     public String solution(String my_string, String alp) {
         String answer = "";
@@ -15,4 +17,4 @@ class Solution {
 
         return answer;
     }
-}
+}*/
