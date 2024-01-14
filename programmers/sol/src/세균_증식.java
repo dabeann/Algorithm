35afdbcd21dd381a116732d8e7fc16e690c3546e
dabeann <dabin4078@daum.net>
2024-01-14 12:57:42 +0900

    programmers: 세균 증식

--- programmers/sol/src/세균_증식.java
+++ programmers/sol/src/세균_증식.java
@@ -1,8 +1,10 @@
-class Solution {
+class 세균_증식 {
     public int solution(int n, int t) {
-        int answer = 0;
+        int answer = n;
 
-
+        for (int i = 0; i < t; i++) {
+            answer*=2;
+        }
 
         return answer;
     }
