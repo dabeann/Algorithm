ffa8114177c231a3868a97929f1e22a6138415b1
dabeann <dabin4078@daum.net>
2024-01-14 12:35:01 +0900

    programmers: 자릿수 더하기

---
+++ programmers/sol/src/자릿수_더하기.java
@@ -0,0 +1,12 @@
+class 자릿수_더하기 {
+    public int solution(int n) {
+        int answer = 0;
+
+        String num = String.valueOf(n);
+        for (char a : num.toCharArray()) {
+            answer += Integer.parseInt(String.valueOf(a));
+        }
+
+        return answer;
+    }
+}
