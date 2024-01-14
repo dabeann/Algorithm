a4831d648c2ac03e2cfb0876fddee7f2bf5001ad
dabeann <dabin4078@daum.net>
2024-01-14 12:23:43 +0900

    feat: 숫자 찾기

--- programmers/sol/src/Main.java
+++ programmers/sol/src/Main.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.util.stream.Collectors;
 
-// 알고리즘 문제
+// 알고리즘 문제 형식
 class Solution {
     public String solution(String my_string, String alp) {
         String answer = "";
@@ -14,17 +14,5 @@ class Solution {
         }
 
         return answer;
-    }
-}
-
-
-//Main 클래스에서 Solution클래스 선언해주기
-public class Main {
-    public static void main(String[] ars) {
-        Solution s = new Solution();
-        //입력요소를 선언해줘야 출력값이 나옴
-
-        //int[] a = {5, 9, 7, 10};
-        //s.solution(a, 5);
     }
 }
---
+++ programmers/sol/src/숫자_찾기.java
@@ -0,0 +1,20 @@
+class 숫자_찾기 {
+    public int solution(int num, int k) {
+        int answer = 1;
+
+        String num1 = Integer.toString(num);
+        char num2 = Integer.toString(k).charAt(0);
+
+        for (char a : num1.toCharArray()) {
+            if (a == num2) {
+                break;
+            }
+            answer++;
+        }
+        if (answer == num1.length()+1) {
+            answer = -1;
+        }
+
+        return answer;
+    }
+}
