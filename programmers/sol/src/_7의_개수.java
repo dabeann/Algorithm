f64ce61f77bc3bdd96e147d098153d1b66a88c36
dabeann <dabin4078@daum.net>
2024-01-17 12:22:06 +0900

    programmers: 7의 개수

---
+++ programmers/sol/src/_7의_개수.java
@@ -0,0 +1,14 @@
+class _7의_개수 {
+    public int solution(int[] array) {
+        int answer = 0;
+
+        for (int a : array) {
+            String st = String.valueOf(a);
+            for (char b : st.toCharArray()) {
+                if (b=='7') answer++;
+            }
+        }
+
+        return answer;
+    }
+}
