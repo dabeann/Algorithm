09d3d33ede40ead0d6c3bbdd4cf59b91daf2dca0
dabeann <dabin4078@daum.net>
2024-01-17 12:40:15 +0900

    programmers: 머쓱이보다 키 큰 사람

---
+++ programmers/sol/src/머쓱이보다_키_큰_사람.java
@@ -0,0 +1,11 @@
+class 머쓱이보다_키_큰_사람 {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+
+        for (int a : array) {
+            if(a>height)answer++;
+        }
+
+        return answer;
+    }
+}
