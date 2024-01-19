642a28a803d012b5a7bd747cc9218f89145714d8
dabeann <dabin4078@daum.net>
2024-01-19 13:20:51 +0900

    programmers: 저주의 숫자 3

---
+++ programmers/sol/src/저주의_숫자_3.java
@@ -0,0 +1,15 @@
+class 저주의_숫자_3 {
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int i = 1; i < n+1; i++) {
+            answer++;
+            while (answer % 3 == 0 || String.valueOf(answer).contains("3")) {
+                answer++;
+            }
+
+        }
+
+        return answer;
+    }
+}
