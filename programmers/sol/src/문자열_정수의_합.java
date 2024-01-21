beea2ff7b86720cad8602c45070e3cf9d41275c0
dabeann <dabin4078@daum.net>
2024-01-21 12:48:09 +0900

    programmers: 문자열 정수의 합

---
+++ programmers/sol/src/문자열_정수의_합.java
@@ -0,0 +1,11 @@
+class 문자열_정수의_합 {
+    public int solution(String num_str) {
+        int answer = 0;
+
+        for (char a : num_str.toCharArray()) {
+            answer += Integer.parseInt(String.valueOf(a));
+        }
+
+        return answer;
+    }
+}
