08a9eac4cba09b2fb5e1754421c1c9b959b7ca61
dabeann <dabin4078@daum.net>
2024-01-21 12:50:34 +0900

    programmers: 0 떼기

---
+++ programmers/sol/src/_0_떼기.java
@@ -0,0 +1,10 @@
+class _0_떼기 {
+    public String solution(String n_str) {
+        String answer = "";
+
+        int i = Integer.parseInt(n_str);
+        answer = String.valueOf(i);
+
+        return answer;
+    }
+}
