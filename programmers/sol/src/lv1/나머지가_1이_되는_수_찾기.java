aa7653110fdcb211a08b2a542ef5ae849b85bfe6
dabeann <dabin4078@daum.net>
2024-02-13 07:00:00 +0900

    programmers lv1: 나머지가 1이 되는 수 찾기

---
+++ programmers/sol/src/lv1/나머지가_1이_되는_수_찾기.java
@@ -0,0 +1,13 @@
+package lv1;
+
+public class 나머지가_1이_되는_수_찾기 {
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (n%i==1) return i;
+        }
+
+        return answer;
+    }
+}
