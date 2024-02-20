78ec9908b539e2e22bbf6a646a811d98202836de
dabeann <dabin4078@daum.net>
2024-02-20 12:25:50 +0900

    programmers lv2: 재구매가 일어난 상품과 회원 리스트 구하기

---
+++ programmers/sol/src/lv2/재구매가_일어난_상품과_회원_리스트_구하기.sql
@@ -0,0 +1,20 @@
+SELECT
+    USER_ID,
+    PRODUCT_ID
+FROM
+    ONLINE_SALE
+WHERE
+        (USER_ID, PRODUCT_ID, SALES_DATE) IN (
+        SELECT
+            USER_ID,
+            PRODUCT_ID,
+            MIN(SALES_DATE) AS first_purchase_date
+        FROM
+            ONLINE_SALE
+        GROUP BY
+            USER_ID, PRODUCT_ID
+        HAVING
+                COUNT(*) > 1
+    )
+ORDER BY
+    USER_ID ASC, PRODUCT_ID DESC;
