9969e21096c133f64b1ec8953d5efd7a50c42d7b
dabeann <dabin4078@daum.net>
2024-02-24 12:13:28 +0900

    programmers lv2: 타겟 넘버

---
+++ programmers/sol/src/lv2/타겟_넘버.java
@@ -0,0 +1,22 @@
+package lv2;
+
+import java.util.*;
+
+public class 타겟_넘버 {
+    int answer = 0;
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, target, 0, 0);
+        return answer;
+    }
+
+    public void dfs(int[] numbers, int target, int sum, int depth) {
+        if (depth == numbers.length) {
+            if (sum == target) {
+                answer++;
+            }
+            return;
+        }
+        dfs(numbers, target, sum + numbers[depth], depth + 1);
+        dfs(numbers, target, sum - numbers[depth], depth + 1);
+    }
+}
