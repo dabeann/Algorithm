923d423649fff9e96ee29796aba258701629d5d0
dabeann <dabin4078@daum.net>
2024-02-25 15:35:36 +0900

    programmers lv1: 소수 만들기

---
+++ programmers/sol/src/lv1/소수_만들기.java
@@ -0,0 +1,26 @@
+package lv1;
+
+public class 소수_만들기 {
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        for (int i = 0; i < nums.length-2; i++) {
+            for (int j = i+1; j < nums.length-1; j++) {
+                for (int k = j+1; k < nums.length; k++) {
+                    if (checkPrime(nums[i]+nums[j]+nums[k])) answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    private boolean checkPrime(int n) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
