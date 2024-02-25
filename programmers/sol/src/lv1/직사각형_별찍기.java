5e5b1297fb4b2ee9cdd8e45bb6b6b023de40aaaa
dabeann <dabin4078@daum.net>
2024-02-25 15:38:42 +0900

    programmers lv1: 직사각형 별찍기

---
+++ programmers/sol/src/lv1/직사각형_별찍기.java
@@ -0,0 +1,15 @@
+package lv1;
+
+import java.util.*;
+
+public class 직사각형_별찍기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for (int i = 0; i < b; i++) {
+            System.out.println("*".repeat(a));
+        }
+    }
+}
