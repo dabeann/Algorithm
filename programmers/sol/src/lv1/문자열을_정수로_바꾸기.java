4069eb52473ca1b38e9ee6458635c49092492034
dabeann <dabin4078@daum.net>
2024-02-26 14:50:19 +0900

    programmers lv1: 문자열을 정수로 바꾸기

---
+++ programmers/sol/src/lv1/문자열을_정수로_바꾸기.java
@@ -0,0 +1,11 @@
+package lv1;
+
+public class 문자열을_정수로_바꾸기 {
+    public int solution(String s) {
+        int answer = 0;
+
+        answer = Integer.valueOf(s);
+
+        return answer;
+    }
+}
