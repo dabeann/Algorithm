5ed3593ba73247ad3c3e94109ab3b75bb6d762ad
dabeann <dabin4078@daum.net>
2024-02-26 16:32:46 +0900

    programmers lv2: JadenCase 문자열 만들기

---
+++ programmers/sol/src/lv2/JadenCase_문자열_만들기.java
@@ -0,0 +1,25 @@
+package lv2;
+
+public class JadenCase_문자열_만들기 {
+    public String solution(String s) {
+        String answer = "";
+
+        String[] arr = s.split(" ");
+        for(int i=0; i<arr.length; i++) {
+            String now = arr[i];
+            if(arr[i].length() == 0) {
+                answer += " ";
+            }
+            else {
+                answer += now.substring(0, 1).toUpperCase();
+                answer += now.substring(1, now.length()).toLowerCase();
+                answer += " ";
+            }
+
+        }
+        if(s.substring(s.length()-1, s.length()).equals(" ")){
+            return answer;
+        }
+        return answer.substring(0, answer.length()-1);
+    }
+}
