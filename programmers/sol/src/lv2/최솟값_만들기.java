a4202a2a102025bdca32bbaa5c5dbb19d7abd396
dabeann <dabin4078@daum.net>
2024-02-26 16:42:09 +0900

    programmers lv2: 최솟값 만들기

---
+++ programmers/sol/src/lv2/최솟값_만들기.java
@@ -0,0 +1,17 @@
+package lv2;
+
+import java.util.*;
+
+public class 최솟값_만들기 {
+    public int solution(int []A, int []B) {
+        int answer = 0;
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+        for (int i = 0; i < A.length; i++) {
+            answer += A[i] * B[B.length - i - 1];
+        }
+
+        return answer;
+    }
+}
