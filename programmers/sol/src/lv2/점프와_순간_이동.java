fb9bc12b44da544b226e30078f03176fc61ac551
dabeann <dabin4078@daum.net>
2024-03-01 13:05:11 +0900

    programmers lv2: 점프와 순간 이동

---
+++ programmers/sol/src/lv2/점프와_순간_이동.java
@@ -0,0 +1,18 @@
+package lv2;
+
+import java.util.*;
+
+public class 점프와_순간_이동 {
+    public int solution(int n) {
+        int ans = 0;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n -= 1;
+                ans += 1;
+            }
+        }
+        return ans;
+    }
+}
