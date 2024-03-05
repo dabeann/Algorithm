c5cea167af0a109159a0697924565798406b3088
dabeann <dabin4078@daum.net>
2024-03-05 14:56:07 +0900

    programmers lv2: n^2 배열 자르기

---
+++ programmers/sol/src/lv2/nxn_배열_자르기.java
@@ -0,0 +1,17 @@
+package lv2;
+
+import java.util.*;
+
+public class nxn_배열_자르기 {
+    public int[] solution(int n, long left, long right) {
+        int[] answer = new int[(int)(right - left) + 1];
+        for (int i = 0; i < answer.length; i++) {
+            int y = (int)(left / n + 1);
+            int x = (int)(left % n + 1);
+            left++;
+            answer[i] = Math.max(y, x);
+        }
+
+        return answer;
+    }
+}
